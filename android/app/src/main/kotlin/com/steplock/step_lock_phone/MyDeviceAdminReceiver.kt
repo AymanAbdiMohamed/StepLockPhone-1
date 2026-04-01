2713ffe47b6a7eeb826fac89e1ac1f0b2d4a4388
AymanAbdiMohamed <9100aymanabdi@gmail.com>
2026-04-01 17:41:10 +0300

    feat: Implement StepLockDeviceAdmin class for device administration support

---
+++ android/app/src/main/kotlin/com/steplock/step_lock_phone/MyDeviceAdminReceiver.kt
@@ -0,0 +1,15 @@
+package com.yourpackage.steplockphone
+
+import android.app.main.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class StepLockDeviceAdmin : DeviceAdminReceiver() {
+    override fun onEnable(context: Context, intent: Intent){
+
+    }
+
+    override fun onDisable(context: Context, intent: Intent) {
+
+    }
+}
